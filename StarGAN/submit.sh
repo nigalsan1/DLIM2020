d2c175926bf13b4c20bcb7c5a4745543581631b9
AyexGG <dennis.vilgertshofer@gmail.com>
2020-11-25 20:01:07 +0100

    First Test

--- StarGAN/submit.sh
+++ StarGAN/submit.sh
@@ -4,9 +4,8 @@
 #SBATCH --mem=40G
 #SBATCH --constraint=titan_x
 source /usr/itetnas04/data-scratch-01/dlim_08hs20/data/conda/etc/profile.d/conda.sh
-conda activate dlim
+conda activate pytcu10
 python -u main.py "$@" --mode train --dataset CelebA --image_size 128 --c_dim 5 \
                --sample_dir stargan_celeba/samples --log_dir stargan_celeba/logs \
                --model_save_dir stargan_celeba/models --result_dir stargan_celeba/results \
-               --selected_attrs Black_Hair Blond_Hair Brown_Hair Male Young --model_save_step 2500 --g_lr 0.0005 \
-               --d_lr 0.0005 --num_iters 50000 --num_iters_decay 30000
+               --log_step 50 --selected_attrs Black_Hair Blond_Hair Brown_Hair Male Young
