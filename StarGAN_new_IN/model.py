a926f9eb9e7c26f594748078f3df0881a3938944
AyexGG <dennis.vilgertshofer@gmail.com>
2020-12-14 21:09:39 +0100

    Updated model.py
    Ready for first debugging

--- StarGAN_new_IN/model.py
+++ StarGAN_new_IN/model.py
@@ -5,17 +5,22 @@ import numpy as np
 
 class ResidualBlock(nn.Module):
     """Residual Block with instance normalization."""
-    def __init__(self, dim_in, dim_out):
+    def __init__(self, dim_in, dim_out, c_dim):
         super(ResidualBlock, self).__init__()
-        self.main = nn.Sequential(
-            nn.Conv2d(dim_in, dim_out, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.InstanceNorm2d(dim_out, affine=True, track_running_stats=True),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(dim_out, dim_out, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.InstanceNorm2d(dim_out, affine=True, track_running_stats=True))
+        self.L1 = nn.Conv2d(dim_in, dim_out, kernel_size=3, stride=1, padding=1, bias=False)
+        self.L2 = ccbn(input_size=c_dim,output_size=dim_out)
+        self.L3 = nn.ReLU(inplace=True)
+        self.L4 = nn.Conv2d(dim_out, dim_out, kernel_size=3, stride=1, padding=1, bias=False)
+        self.L5 = ccbn(input_size=c_dim,output_size=dim_out)
 
-    def forward(self, x):
-        return x + self.main(x)
+    def forward(self, x, c):
+        x0 = x
+        x = self.L1(x)
+        x = self.L2(x,c)
+        x = self.L3(x)
+        x = self.L4(x)
+        x = self.L5(x,c)
+        return x0 + x
 
 class ccbn(nn.Module): #Not sure if the nn.Module is needed
     def __init__(self, output_size, input_size, eps=1e-5, momentum=0.1):
@@ -53,38 +58,37 @@ class Generator(nn.Module):
         super(Generator, self).__init__()
 
         self.L1 = nn.Conv2d(3+c_dim, conv_dim, kernel_size=7, stride=1, padding=3, bias=False)
-        self.L2 = ccbn() #Input & Output Size?
+        self.L2 = ccbn(output_size = conv_dim, input_size = c_dim) #Input & Output Size?
         self.L3 = nn.ReLU(inplace=True)
 
         # Down-sampling layers.
         curr_dim = conv_dim
         self.L4 = nn.Conv2d(curr_dim, curr_dim*2, kernel_size=4, stride=2, padding=1, bias=False)
-        self.L5 = ccbn() #Input & Output Size?
+        self.L5 = ccbn(output_size = curr_dim*2, input_size = c_dim) 
         self.L6 = nn.ReLU(inplace=True)
 
         curr_dim = curr_dim * 2
 
         self.L7 = nn.Conv2d(curr_dim, curr_dim*2, kernel_size=4, stride=2, padding=1, bias=False)
-        self.L8 = ccbn() #Input & Output Size?
+        self.L8 = ccbn(output_size = curr_dim*2, input_size = c_dim)
         self.L9 = nn.ReLU(inplace=True)
 
         #Bottleneck Layers
-        self.L10 = ResidualBlock(dim_in=curr_dim, dim_out=curr_dim)
-        self.L11 = ResidualBlock(dim_in=curr_dim, dim_out=curr_dim)
-        self.L12 = ResidualBlock(dim_in=curr_dim, dim_out=curr_dim)
-        self.L13 = ResidualBlock(dim_in=curr_dim, dim_out=curr_dim)
-        self.L14 = ResidualBlock(dim_in=curr_dim, dim_out=curr_dim)
-        self.L15 = ResidualBlock(dim_in=curr_dim, dim_out=curr_dim)
+        self.L10 = ResidualBlock(dim_in=curr_dim, dim_out=curr_dim, c_dim=c_dim)
+        self.L12 = ResidualBlock(dim_in=curr_dim, dim_out=curr_dim, c_dim=c_dim)
+        self.L13 = ResidualBlock(dim_in=curr_dim, dim_out=curr_dim, c_dim=c_dim)
+        self.L14 = ResidualBlock(dim_in=curr_dim, dim_out=curr_dim, c_dim=c_dim)
+        self.L15 = ResidualBlock(dim_in=curr_dim, dim_out=curr_dim, c_dim=c_dim)
 
         # Up-sampling layers.
         self.L16 = nn.ConvTranspose2d(curr_dim, curr_dim//2, kernel_size=4, stride=2, padding=1, bias=False)
-        self.L17 = ccbn() #Input & Output Size?
+        self.L17 = ccbn(output_size = curr_dim//2, input_size = c_dim)
         self.L18 = nn.ReLU(inplace=True)
 
         curr_dim = curr_dim // 2
 
         self.L19 = nn.ConvTranspose2d(curr_dim, curr_dim//2, kernel_size=4, stride=2, padding=1, bias=False)
-        self.L20 = ccbn() #Input & Output Size?
+        self.L20 = ccbn(output_size = curr_dim//2, input_size = c_dim) 
         self.L21 = nn.ReLU(inplace=True)
         
         self.L22 = nn.Conv2d(curr_dim, 3, kernel_size=7, stride=1, padding=3, bias=False)
@@ -94,41 +98,38 @@ class Generator(nn.Module):
         # Replicate spatially and concatenate domain information.
         # Note that this type of label conditioning does not work at all if we use reflection padding in Conv2d.
         # This is because instance normalization ignores the shifting (or bias) effect.
-        c = c.view(c.size(0), c.size(1), 1, 1)
-        c = c.repeat(1, 1, x.size(2), x.size(3))
-        x = torch.cat([x, c], dim=1)
         
-        self.L1(x)
-        self.L2(x,c)
-        self.L3(x)
+        x = self.L1(x)
+        x = self.L2(x,c)
+        x = self.L3(x)
 
         # Down-sampling layers.
-        self.L4(x)
-        self.L5(x,c)
-        self.L6(x)
-        self.L7(x)
-        self.L8(x,c)
-        self.L9(x)
+        x = self.L4(x)
+        x = self.L5(x,c)
+        x = self.L6(x)
+        x = self.L7(x)
+        x = self.L8(x,c)
+        x = self.L9(x)
 
         #Bottleneck Layers
-        self.L10(x)
-        self.L11(x)
-        self.L12(x)
-        self.L13(x)
-        self.L14(x)
-        self.L15(x)
+        x = self.L10(x)
+        x = self.L11(x)
+        x = self.L12(x)
+        x = self.L13(x)
+        x = self.L14(x)
+        x = self.L15(x)
 
         # Up-sampling layers.
-        self.L16(x)
-        self.L17(x,c)
-        self.L18(x)
-        self.L19(x)
-        self.L20(x,c)
-        self.L21(x)
+        x = self.L16(x)
+        x = self.L17(x,c)
+        x = self.L18(x)
+        x = self.L19(x)
+        x = self.L20(x,c)
+        x = self.L21(x)
 
-        self.L22(x)
-        self.L23(x)
-        return
+        x = self.L22(x)
+        x = self.L23(x)
+        return x
 
 
 class Discriminator(nn.Module):
