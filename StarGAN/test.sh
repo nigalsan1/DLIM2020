f9d5e3d0f064253b84a611591cddf14a902b10f5
nigalsan1 <nigalsan@gmx.ch>
2020-12-14 22:10:16 +0100

    Update test.sh

--- StarGAN/test.sh
+++ StarGAN/test.sh
@@ -5,8 +5,8 @@
 #SBATCH --constraint=titan_x
 source /usr/itetnas04/data-scratch-01/dlim_07hs20/data/conda/etc/profile.d/conda.sh
 conda activate pytcu10
-python -u main.py "$@" --mode test --dataset RaFD --rafd_crop_size CROP_SIZE --image_size IMG_SIZE \
-               --c_dim LABEL_DIM --rafd_image_dir TEST_IMG_DIR \
+python -u main.py "$@" --mode test --dataset RaFD --rafd_crop_size 256 --image_size 256 \
+               --c_dim 5 --rafd_image_dir data/RaFD/test/happy \
                --sample_dir stargan_custom/samples --log_dir stargan_custom/logs \
                --model_save_dir stargan_custom/models --result_dir stargan_custom/results
                
