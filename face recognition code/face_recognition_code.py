1a8acce7824ebb5c32d825c340e4806f3df180f9
flaviose <67025825+flaviose@users.noreply.github.com>
2020-12-16 17:22:49 +0100

    Create face_recognition_code.py

---
+++ face recognition code/face_recognition_code.py
@@ -0,0 +1,30 @@
+import cv2
+
+# Load the cascade
+face_cascade = cv2.CascadeClassifier('C:\\Users\\flavi\\anaconda3\\Library\\etc\\haarcascades\\haarcascade_frontalface_default.xml')
+
+# To capture video from webcam. 
+cap = cv2.VideoCapture(0)
+# To use a video file as input 
+# cap = cv2.VideoCapture('filename.mp4')
+
+while True:
+    # Read the frame
+    ret, img = cap.read()
+    if ret == True:
+        # Convert to grayscale
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # Detect the faces
+        faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+        # Draw the rectangle around each face
+        for (x, y, w, h) in faces:
+            cv2.rectangle(img, (x, y-50), (x+w+20, y+h+80), (255, 0, 0), 2)
+        # Display
+        cv2.imshow('img', img)
+        # Stop if escape key is pressed
+        k = cv2.waitKey(30) & 0xff
+        if k==27:
+            break
+# Release the VideoCapture object
+cap.release()
+cv2.destroyAllWindows()
