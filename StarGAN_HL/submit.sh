b3f7d6f7472ee94de2d5a49e532c2c0db0e7a004
AyexGG <dennis.vilgertshofer@gmail.com>
2020-12-16 21:16:46 +0100

    Added a bunch of things

---
+++ StarGAN_HL/submit.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --output=log/%j.out
+#SBATCH --gres=gpu:1
+#SBATCH --mem=40G
+#SBATCH --constraint=titan_x
+source /usr/itetnas04/data-scratch-01/dlim_08hs20/data/conda/etc/profile.d/conda.sh
+conda activate pytcu10
+python -u main.py "$@" --mode train --dataset CelebA --image_size 128 --c_dim 5 \
+               --sample_dir ../../Images/G-LR_0.00001 --log_dir stargan_celeba/logs \
+               --model_save_dir stargan_celeba/models --result_dir stargan_celeba/results \
+               --log_step 50 --selected_attrs Black_Hair Blond_Hair Brown_Hair Male Young --model_save_step 50000 \
+               --batch_size 16 \
+               --celeba_image_dir ../Datasets/celeba/images \
+               --attr_path ../Datasets/celeba/list_attr_celeba.txt
