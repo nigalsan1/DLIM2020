c1dcdec0b13d0984a0459f319848e5d9a63ff667
AyexGG <dennis.vilgertshofer@gmail.com>
2020-12-16 20:27:58 +0100

    Revert "Revert "Update download.sh""
    This reverts commit 423d443125c7b68cf0d6cc76b3d0f117234a092a.

--- download.sh
+++ download.sh
@@ -1,13 +1,26 @@
-URL=https://www.dropbox.com/s/h8h2mebvugsppzn/test2.zip?dl=0
-ZIP_FILE=./Models/Models.zip
-mkdir -p ./Models/
-wget -N $URL -O $ZIP_FILE
-unzip $ZIP_FILE -d ./Models/
-rm $ZIP_FILE
+FILE=$1
 
-URL=https://www.dropbox.com/s/d1kjpkqklf0uw77/celeba.zip?dl=0
-ZIP_FILE=./Datasets/celeba.zip
-mkdir -p ./Datasets/
-wget -N $URL -O $ZIP_FILE
-unzip $ZIP_FILE -d ./StarGAN/data/
-rm $ZIP_FILE
+if [ $FILE == "celeba" ]; then
+
+    # CelebA images and attribute labels
+    URL=https://www.dropbox.com/s/d1kjpkqklf0uw77/celeba.zip?dl=0
+    ZIP_FILE=./Datasets/celeba.zip
+    mkdir -p ./Datasets/
+    wget -N $URL -O $ZIP_FILE
+    unzip $ZIP_FILE -d ./Datasets/
+    rm $ZIP_FILE
+
+
+elif [ $FILE == 'models' ]; then
+
+    # Models that have been trained during this project
+    URL=https://www.dropbox.com/s/fgc5wnql9o7u3sd/Models.zip?dl=0
+    ZIP_FILE=./Models.zip
+    wget -N $URL -O $ZIP_FILE
+    unzip $ZIP_FILE 
+    rm $ZIP_FILE
+
+else
+    echo "Available arguments are celeba and models"
+    exit 1
+fi
